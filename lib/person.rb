04f73aecd7b9332f0bd7808bd6259391e6a3f1d1
Rodgers Ogada <ogadarodgers@gmail.com>
2023-02-09 15:20:59 +0300

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
+    #setter method for name
+    def name=(name)
+        @name = name
+    end
+
+    #getter method for name
+    def name
+        @name
+    end
+
+    # setter method for job
+    def job=(job)
+        @job=job
+    end
+
+    #getter method for job
+    def job
+        @job
+    end
 
 end
